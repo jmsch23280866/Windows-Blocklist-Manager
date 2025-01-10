350b09cc82f8070d12e779bb446b5f0f0bf2eca1
Agent_E04 <58344071+jmsch23280866@users.noreply.github.com>
2025-01-10 10:10:18 +0800

    Delete (以管理員執行)清除當前使用者禁止執行清單.bat

--- (以管理員執行)清除當前使用者禁止執行清單.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set key="HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\DisallowRun"
-
-for /f "tokens=*" %%A in ('reg query %key% /v * 2^>nul ^| find /i "REG_SZ"') do (
-    for /f "tokens=1" %%B in ("%%A") do reg delete %key% /v %%B /f
-)
-echo 當前使用者所有禁止執行項目已被刪除！
-pause
